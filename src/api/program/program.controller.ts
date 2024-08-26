0e3267a00f5660d2caa9dc5751a5fd71d80205f5
Fozil Mamadaliev <fmcoder23@gmail.com>
2024-08-26 13:05:39 +0500

    implemented guards in university and program modules

--- src/api/program/program.controller.ts
+++ src/api/program/program.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { ProgramService } from './program.service';
 import { CreateProgramDto } from './dto/create-program.dto';
 import { UpdateProgramDto } from './dto/update-program.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/role.decorator';
+import { Role } from 'src/common/enums/role.enum';
 
 @ApiTags("Program")
+@ApiBearerAuth()
+@UseGuards(RolesGuard)
 @Controller('program')
 export class ProgramController {
   constructor(private readonly programService: ProgramService) {}
 
   @Post()
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   create(@Body() createProgramDto: CreateProgramDto) {
     return this.programService.create(createProgramDto);
   }
@@ -25,11 +31,13 @@ export class ProgramController {
   }
 
   @Put(':id')
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   update(@Param('id') id: string, @Body() updateProgramDto: UpdateProgramDto) {
     return this.programService.update(id, updateProgramDto);
   }
 
   @Delete(':id')
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   remove(@Param('id') id: string) {
     return this.programService.remove(id);
   }
--- src/api/university/university.controller.ts
+++ src/api/university/university.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { UniversityService } from './university.service';
 import { CreateUniversityDto } from './dto/create-university.dto';
 import { UpdateUniversityDto } from './dto/update-university.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/role.decorator';
+import { Role } from 'src/common/enums/role.enum';
 
 @ApiTags("University")
+@ApiBearerAuth()
+@UseGuards(RolesGuard)
 @Controller('university')
 export class UniversityController {
   constructor(private readonly universityService: UniversityService) {}
 
   @Post()
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   create(@Body() createUniversityDto: CreateUniversityDto) {
     return this.universityService.create(createUniversityDto);
   }
@@ -25,11 +31,13 @@ export class UniversityController {
   }
 
   @Put(':id')
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   update(@Param('id') id: string, @Body() updateUniversityDto: UpdateUniversityDto) {
     return this.universityService.update(id, updateUniversityDto);
   }
 
   @Delete(':id')
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   remove(@Param('id') id: string) {
     return this.universityService.remove(id);
   }
